ddaa66ad38dcd13eb39fdc3bb736c53aeda0a649
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2022-08-24 22:08:44 +0530

    Update RevOfStr.cpp

--- RevOfStr.cpp
+++ RevOfStr.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void rev(string &str, int s, int e){
+void rev(string &str, int s){
+	int e = str.length() - 1 - s;
 	if(s > e) return ;
 	
 	swap(str[s++], str[e--]);
 	
-	rev(str,s,e);
+	rev(str,s);
 }
 
 int main(){
 	string s = "abcdefg";
-	rev(s,0,s.length() - 1);
+	rev(s,0);
 	cout<<"\t"<<s;
 	
 	return 0;
